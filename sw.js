236c7c2e0792067cbd0bb489c511274d5a7edfbd
114040-max <114040@ccjh.cyc.edu.tw>
2026-01-15 17:50:31 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,21 @@
-self.addEventListener('install', event => {
+const CACHE_NAME = "whispering-pines-v1";
+
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest"
+];
+
+self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
   );
 });
